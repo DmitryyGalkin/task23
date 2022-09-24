abc67f78e6ebdecc816503317cc1b2d834236547
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-24 15:58:18 +0300

    Возвведение в куб от 1 до N

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (count = 1; count <= num; count++)
+{
+    Console.Write(Math.Pow(count, 3));
+    if (count != num)
+    {
+        Console.Write(", ");
+    }
+}
